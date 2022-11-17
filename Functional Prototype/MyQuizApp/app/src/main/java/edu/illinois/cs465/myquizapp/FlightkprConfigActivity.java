cd5460e1f88f215fe72d411d03ec45ab4957f818
yijut2 <52221089+yijut2@users.noreply.github.com>
2022-11-17 14:08:39 -0600

    Update FlightkprConfigActivity.java

--- Functional Prototype/MyQuizApp/app/src/main/java/edu/illinois/cs465/myquizapp/FlightkprConfigActivity.java
+++ Functional Prototype/MyQuizApp/app/src/main/java/edu/illinois/cs465/myquizapp/FlightkprConfigActivity.java
@@ -11,6 +11,9 @@ import android.widget.EditText;
 import edu.illinois.cs465.myquizapp.FlightKeeperDetailActivity;
 import edu.illinois.cs465.myquizapp.R;
 
+import edu.illinois.cs465.myquizapp.pojo.Filter;
+import edu.illinois.cs465.myquizapp.Database;
+
 public class FlightkprConfigActivity extends AppCompatActivity implements View.OnClickListener {
     private Button advanced_op;
     private Button confirm;
@@ -18,6 +21,14 @@ public class FlightkprConfigActivity extends AppCompatActivity implements View.O
     private EditText to_location;
     private EditText lower_bound;
     private EditText upper_bound;
+    
+    private Integer bags;
+    private Integer stops;
+    private Integer duration;
+    private Integer adult_cnt;
+    private Integer children_cnt;
+    private Integer infant_cnt;
+    private String collectionName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,19 @@ public class FlightkprConfigActivity extends AppCompatActivity implements View.O
         to_location = (EditText)findViewById(R.id.to_location);
         lower_bound = (EditText)findViewById(R.id.lower);
         upper_bound = (EditText)findViewById(R.id.upper);
+        
+        Intent intent = getIntent();
+        if (intent.getStringExtra("lastpage").equals("details")) {
+            collectionName = intent.getStringExtra("collectionName");
+        }
+        else if (intent.getStringExtra("lastpage").equals("advanced")) {
+            bags = intent.getIntExtra("bags", 0);
+            stops = intent.getIntExtra("stops", 0);
+            duration = intent.getIntExtra("duration", 0);
+            adult_cnt = intent.getIntExtra("adult_cnt", 0);
+            children_cnt = intent.getIntExtra("children_cnt", 0);
+            infant_cnt = intent.getIntExtra("infant_cnt", 0);
+        }
 
     }
 
@@ -44,10 +68,25 @@ public class FlightkprConfigActivity extends AppCompatActivity implements View.O
             startActivity(intent);
         }
         else if (v.getId() == R.id.confirm){
-            Number low = Integer.parseInt(lower_bound.getText().toString());
-            Number upper = Integer.parseInt(upper_bound.getText().toString());
+            Integer low = Integer.parseInt(lower_bound.getText().toString());
+            Integer upper = Integer.parseInt(upper_bound.getText().toString());
             String from = from_location.getText().toString();
             String to = to_location.getText().toString();
+            
+            Filter curr_filter = new Filter();
+            curr_filter.origin = from;
+            curr_filter.destination = to;
+            curr_filter.duration = duration;
+            curr_filter.lowPrice = low;
+            curr_filter.highPrice = upper;
+            curr_filter.infant_cnt = infant_cnt;
+            curr_filter.children_cnt = children_cnt;
+            curr_filter.adult_cnt = adult_cnt;
+            curr_filter.bags = bags;
+            curr_filter.stops = stops;
+            
+            Database.addAutoFilter(collectionName, curr_filter);
+            System.out.println(Database.autoFilter);
 
             Intent intent = new Intent(FlightkprConfigActivity.this, FlightKeeperDetailActivity.class);
             intent.putExtra("lower_bound", low);
