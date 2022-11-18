bf4d2178c56f2c90bb4f765e671d45de1e2ef1f3
yijut2 <52221089+yijut2@users.noreply.github.com>
2022-11-17 21:50:35 -0600

    Update Filter.java

--- Functional Prototype/MyQuizApp/app/src/main/java/edu/illinois/cs465/myquizapp/pojo/Filter.java
+++ Functional Prototype/MyQuizApp/app/src/main/java/edu/illinois/cs465/myquizapp/pojo/Filter.java
@@ -1,16 +1,19 @@
 package edu.illinois.cs465.myquizapp.pojo;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 public class Filter {
-    public String origin;
-    public String destination;
-    public Integer stops;
-    public Integer bags;
-    public Integer duration;
-    public Integer adult_cnt;
-    public Integer children_cnt;
-    public Integer infant_cnt;
-    public Integer lowPrice;
-    public Integer highPrice;
+    public String origin = "";
+    public String destination = "";
+    public Integer stops = 0;
+    public Integer bags = 0;
+    public Integer duration = 0;
+    public Integer adult_cnt = 0;
+    public Integer children_cnt = 0;
+    public Integer infant_cnt = 0;
+    public Integer lowPrice = 0;
+    public Integer highPrice = 0;
 }
