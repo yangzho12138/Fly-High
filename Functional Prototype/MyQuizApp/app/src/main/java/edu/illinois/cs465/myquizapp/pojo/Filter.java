ef63ff64a74af2f0d71f5b695ecc8bdaa4d79ba7
yijut2 <yijut2@uci.edu>
2022-11-28 14:31:13 -0600

    change Filter.java

--- Functional Prototype/MyQuizApp/app/src/main/java/edu/illinois/cs465/myquizapp/pojo/Filter.java
+++ Functional Prototype/MyQuizApp/app/src/main/java/edu/illinois/cs465/myquizapp/pojo/Filter.java
@@ -3,15 +3,15 @@ package edu.illinois.cs465.myquizapp.pojo;
 import java.io.Serializable;
 import java.util.List;
 
-public class Filter implements Serializable {
-    public String origin;
-    public String destination;
-    public Integer stops;
-    public Integer bags;
-    public Integer duration;
-    public Integer adult_cnt;
-    public Integer children_cnt;
-    public Integer infant_cnt;
-    public Integer lowPrice;
-    public Integer highPrice;
+public class Filter {
+    public String origin = "";
+    public String destination = "";
+    public Integer stops = 0;
+    public Integer bags = 0;
+    public Integer duration = 0;
+    public Integer adult_cnt = 0;
+    public Integer children_cnt = 0;
+    public Integer infant_cnt = 0;
+    public Integer lowPrice = 0;
+    public Integer highPrice = 0;
 }
