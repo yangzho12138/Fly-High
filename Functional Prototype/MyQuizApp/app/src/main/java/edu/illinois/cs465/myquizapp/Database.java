299f78b3a6cba31a85cd28b29d83cc0f2c41974c
yz96 <68858113+yangzho12138@users.noreply.github.com>
2022-11-28 12:16:35 -0600

    collection status

--- Functional Prototype/MyQuizApp/app/src/main/java/edu/illinois/cs465/myquizapp/Database.java
+++ Functional Prototype/MyQuizApp/app/src/main/java/edu/illinois/cs465/myquizapp/Database.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import edu.illinois.cs465.myquizapp.pojo.CollectionStatus;
 import edu.illinois.cs465.myquizapp.pojo.Filter;
 import edu.illinois.cs465.myquizapp.pojo.Flight;
 
@@ -14,6 +15,7 @@ public class Database {
     public static Map<String, Set<Flight>> collections = new HashMap<>(); // collectionName, Flights in this collection
     public static Map<String, Map<String, Set<Flight>>> combinationsInCollection = new HashMap<>(); // collectionName, <combinationName, Flights in this combination>
     public static Map<String, Filter> autoFilter = new HashMap<>(); // collectionName, filter
+    public static Map<String, CollectionStatus> status = new HashMap<>(); // collectionName, collectionStatus
 
     static {
         Set<Flight> set = new HashSet<>();
@@ -61,7 +63,15 @@ public class Database {
             flights.add(flight);
             combinations.put(combinationName, flights);
         }
-
+        CollectionStatus cs = status.get(collectionName);
+        if(cs == null) {
+            cs.setLowestPrice(flight.getTotalPrice());
+            cs.setPlanNum(0);
+        }else if(Double.parseDouble(cs.getLowestPrice()) > Double.parseDouble(flight.getTotalPrice())){
+            cs.setLowestPrice(flight.getTotalPrice());
+        }
+        cs.setPlanNum(cs.getPlanNum() + 1);
+        status.put(collectionName, cs);
         combinationsInCollection.put(collectionName, combinations);
     }
 
